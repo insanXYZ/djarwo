bde8f2a3ecca00c966470dd953e5a3ba6d00999d
insanXYZ <insan.nazal@gmail.com>
2024-08-22 11:32:10 +0700

    djarwo theme

---
+++ theme.bash
@@ -0,0 +1,84 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Default Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#1E2128'
+foreground='#ABB2BF'
+color0='#32363D'
+color1='#E06B74'
+color2='#98C379'
+color3='#E5C07A'
+color4='#62AEEF'
+color5='#C778DD'
+color6='#55B6C2'
+color7='#ABB2BF'
+color8='#50545B'
+color9='#EA757E'
+color10='#A2CD83'
+color11='#EFCA84'
+color12='#6CB8F9'
+color13='#D282E7'
+color14='#5FC0CC'
+color15='#B5BCC9'
+
+accent='#5294E2'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrainsMono Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='JetBrainsMono Nerd Font 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='14'
+
+# Geany
+geany_colors='arc.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='JetBrainsMono Nerd Font 10'
+gtk_theme='Dracula'
+icon_theme='Luv-Folders'
+cursor_theme='Vimix'
+
+# Openbox
+ob_theme='Dracula'
+ob_layout='DLIMC'
+ob_font='JetBrainsMono Nerd Font'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='8'
+ob_margin_b='61'
+ob_margin_l='8'
+ob_margin_r='8'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x10'
+dunst_origin='top-right'
+dunst_font='JetBrainsMono Nerd Font 10'
+dunst_border='0'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='6'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
